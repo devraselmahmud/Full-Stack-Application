930ea74caf8f38f5c7054bae03a1195a9296cb2e
Md. Rasel Mahmud <20-1254-h@student.aiub.edu>
2023-12-21 03:26:32 -0800

    initital commit

--- Full-Stack-Angular/src/app/app-routing.module.ts
+++ Full-Stack-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostCustomerComponent } from './components/post-customer/post-customer.component';
+import { GetAllCustomersComponent } from './components/get-all-customers/get-all-customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'customer', component:PostCustomerComponent},
+  {path: 'getcust', component:GetAllCustomersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
